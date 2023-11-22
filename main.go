19e02d9a7179a47f36c4c7f0555e988a8940628c
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-11-22 21:39:06 +0300

    HTTP Requests Handler

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/sirupsen/logrus"
 )
@@ -26,5 +29,16 @@ func main() {
 	// Start a separate routine to process failed events.
 	for i := 0; i < 10; i++ {
 		go ProcessFailedEvents(i)
+	}
+
+	// Set up our HTTP server.
+	http.HandleFunc("/ingest", IngestEventHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Starting server on :%s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Server exited with error: %v", err)
 	}
 }
