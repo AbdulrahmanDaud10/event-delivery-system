6280f14a1b6c4b1d9053a5b1c87ee0fc11134e38
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2023-11-22 09:35:56 +0300

    Redis Connection Setup Execution

---
+++ initialize_redis.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// InitializingRedis Sets up a connection to the Redis Server
+func InitializingRedis() {
+	myRedis := "myRedis:9999" // Redis Server Address
+	client := redis.NewClient(&redis.Options{
+		Addr:     myRedis,
+		Password: "",
+		DB:       0,
+	})
+
+	fmt.Println("Initialize Redis", myRedis)
+	rdb = &RedisClientWrapper{Client: client}
+
+	_, err := rdb.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Error initializing Redis: %v", err)
+	}
+}
